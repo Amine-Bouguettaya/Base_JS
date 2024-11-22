65252ad7e299d83aeba7ffcc70cd6cb9dfa5c1dd
Amine-Bouguettaya <aminebouguettaya5@gmail.com>
2024-11-22 16:56:16 +0100

    add favorite quotes + localstorage

--- Ex_5/js/main.js
+++ Ex_5/js/main.js
@@ -1,8 +1,21 @@
 const board = document.querySelector("#board");
 for (const author in quotes) {
     const box = document.createElement('div');
-    box.classList.add("box") ;
-
+    box.classList.add("box");
+    const img = document.createElement("img");
+    img.src = "assets/star_empty.png";
+    img.addEventListener("click", function() {
+        const isActive = img.classList.contains("active");
+        if (isActive) {
+            img.src = "assets/star_empty.png";
+            localStorage.removeItem("newQuotes");
+        }
+        else {
+            img.src = "assets/star.png";
+            localStorage.setItem("newQuotes", quotes);
+        }
+        img.classList.toggle("active");
+    })
     const text = document.createElement('em');
     text.innerHTML = `“${quotes[author]}”`;
 
@@ -11,5 +24,6 @@ for (const author in quotes) {
 
     box.appendChild(text);
     box.appendChild(quoteAuthor);
+    box.appendChild(img);
     board.appendChild(box);
-    }
+    } 
